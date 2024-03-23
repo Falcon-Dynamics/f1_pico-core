6f55cd36e5c2e4cb53fd578f361e07fbd7ee390e
birdpump <birdpump@gmail.com>
2024-03-23 13:29:15 -0700

    add ADXL345 accelerometer

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,23 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#include "ADXL345_RP2040/ADXL345.h"
+
+
+ADXL345 accelerometer;
+
+void read_sensorsTask(void *pvParameters) {
+    while (true) {
+        printf("X: %d Y: %d Z: %d\n",
+               accelerometer.getX(),
+               accelerometer.getY(),
+               accelerometer.getZ()
+        );
+
+        vTaskDelay(50);
+    }
+}
+
 void led_task(void *pvParameters) {
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
@@ -15,8 +32,14 @@ void led_task(void *pvParameters) {
     }
 }
 
+void setup() {
+    accelerometer.begin(ADXL345_DEFAULT_ADDRESS, i2c_default, PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN);
+    accelerometer.setRange(ADXL345_RANGE_16_G); // set 16 g range
+}
+
 int main() {
     stdio_init_all();
+    setup();
 
     xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
     vTaskStartScheduler();

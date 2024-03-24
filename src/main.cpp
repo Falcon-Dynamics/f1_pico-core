d1264eaa142d65207390d360ca3b8cfbac350643
birdpump <birdpump@gmail.com>
2024-03-24 06:33:31 -0700

    add sd tests

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,13 @@
 #include "pico/stdlib.h"
 
 #include "ADXL345.h"
-#include "ff.h"
 
-FATFS fs;
+#include "f_util.h"
+#include "ff.h"
+#include "pico/stdlib.h"
+#include "rtc.h"
+#include "hw_config.h"
+
 
 ADXL345 accelerometer;
 
@@ -34,6 +38,26 @@ void led_task(void *pvParameters) {
     }
 }
 
+void test_SD() {
+    time_init();
+    FATFS fs;
+    FRESULT fr = f_mount(&fs, "", 1);
+    if (FR_OK != fr) panic("f_mount error: %s (%d)\n", FRESULT_str(fr), fr);
+    FIL fil;
+    const char *const filename = "filename.txt";
+    fr = f_open(&fil, filename, FA_OPEN_APPEND | FA_WRITE);
+    if (FR_OK != fr && FR_EXIST != fr)
+        panic("f_open(%s) error: %s (%d)\n", filename, FRESULT_str(fr), fr);
+    if (f_printf(&fil, "Hello, world!\n") < 0) {
+        printf("f_printf failed\n");
+    }
+    fr = f_close(&fil);
+    if (FR_OK != fr) {
+        printf("f_close error: %s (%d)\n", FRESULT_str(fr), fr);
+    }
+    f_unmount("");
+}
+
 void setup() {
     accelerometer.begin(ADXL345_DEFAULT_ADDRESS, i2c_default, PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN);
     accelerometer.setRange(ADXL345_RANGE_16_G); // set 16 g range
@@ -42,6 +66,8 @@ void setup() {
 int main() {
     stdio_init_all();
     setup();
+
+    test_SD(); // todo test the SD
 
     xTaskCreate(read_sensorsTask, "read_sensorsTask", 256, NULL, 1, NULL);
     vTaskStartScheduler();

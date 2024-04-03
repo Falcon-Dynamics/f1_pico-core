373e30bde046b8543eda79aba21afa407b4ce81a
birdpump <birdpump@gmail.com>
2024-04-03 12:18:37 -0700

    nanopb test

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <sstream>
+
 #include "FreeRTOS.h"
 #include "task.h"
 #include <cstdio>
@@ -79,6 +82,33 @@ void test_SD() {
         printf("f_close error: %s (%d)\n", FRESULT_str(fr), fr);
     }
     f_unmount("");
+}
+
+void testPB(){
+    uint8_t buffer[128];
+    size_t message_length;
+    bool status;
+
+    SimpleMessage message = SimpleMessage_init_zero;
+
+    /* Create a stream that will write to our buffer. */
+    pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+
+    /* Fill in the lucky number */
+    message.lucky_number = 13;
+
+    /* Now we are ready to encode the message! */
+    status = pb_encode(&stream, SimpleMessage_fields, &message);
+    message_length = stream.bytes_written;
+
+    std::stringstream ss;
+    pb_ostream_t ostream = pb_ostream_from_buffer(reinterpret_cast<pb_byte_t *>(&ss),
+                                                  reinterpret_cast<size_t>(pb_encode_string));
+    if (!pb_encode_string(&ostream, buffer, message_length)) {
+        std::cerr << "Encoding to string failed" << std::endl;
+    }
+    std::string encoded_string = ss.str();
+    std::cout << "Encoded string: " << encoded_string << std::endl;
 }
 
 void setup() {
